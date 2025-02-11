58b98f9f71c97ac589cabb65a102655333084ee4
suraj patel <sp9589890205@gmail.com>
2025-02-11 11:55:57 +0530

    [Suraj] Add. Data Scientist class for AI-Driven Resume Screening System

---
+++ Day_01/src/main/java/com/generics/airesumescreening/DataScientist.java
@@ -0,0 +1,22 @@
+package com.generics.airesumescreening;
+
+
+
+// Data Scientist Role
+class DataScientist extends JobRole {
+    private String tool;
+
+
+    public DataScientist(String candidateName, int experienceYears, String tool) {
+        super(candidateName, experienceYears);
+        this.tool = tool;
+    }
+
+
+    @Override
+    public void displayResume() {
+        System.out.println("Data Scientist: " + getCandidateName() +
+                " | Experience: " + getExperienceYears() + " years" +
+                " | Tool: " + tool);
+    }
+}

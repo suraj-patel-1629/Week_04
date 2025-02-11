9ad8b43b6e0f31a817fd5c876c35ae67a63946a1
suraj patel <sp9589890205@gmail.com>
2025-02-11 11:57:09 +0530

    [Suraj] Add. Software Engineer class for AI-Driven Resume Screening System

---
+++ Day_01/src/main/java/com/generics/airesumescreening/SoftwareEngineer.java
@@ -0,0 +1,22 @@
+package com.generics.airesumescreening;
+
+import com.generics.airesumescreening.JobRole;
+
+// Software Engineer Role
+class SoftwareEngineer extends JobRole {
+    private String programmingLanguage;
+
+
+    public SoftwareEngineer(String candidateName, int experienceYears, String programmingLanguage) {
+        super(candidateName, experienceYears);
+        this.programmingLanguage = programmingLanguage;
+    }
+
+
+    @Override
+    public void displayResume() {
+        System.out.println("Software Engineer: " + getCandidateName() +
+                " | Experience: " + getExperienceYears() + " years" +
+                " | Language: " + programmingLanguage);
+    }
+}

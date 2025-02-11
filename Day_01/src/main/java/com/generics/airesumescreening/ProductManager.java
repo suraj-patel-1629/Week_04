32e8693ec5b8180219d39a4bfcc2eb1b7285d307
suraj patel <sp9589890205@gmail.com>
2025-02-11 11:56:29 +0530

    [Suraj] Add. Product manager class for AI-Driven Resume Screening System

---
+++ Day_01/src/main/java/com/generics/airesumescreening/ProductManager.java
@@ -0,0 +1,22 @@
+package com.generics.airesumescreening;
+
+import com.generics.airesumescreening.JobRole;
+
+// Product Manager Role
+class ProductManager extends JobRole {
+    private int projectsManaged;
+
+
+    public ProductManager(String candidateName, int experienceYears, int projectsManaged) {
+        super(candidateName, experienceYears);
+        this.projectsManaged = projectsManaged;
+    }
+
+
+    @Override
+    public void displayResume() {
+        System.out.println("Product Manager: " + getCandidateName() +
+                " | Experience: " + getExperienceYears() + " years" +
+                " | Projects: " + projectsManaged);
+    }
+}

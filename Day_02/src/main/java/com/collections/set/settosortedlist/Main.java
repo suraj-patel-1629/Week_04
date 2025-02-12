fe2561ffba2bd782c58c9d32bae5597740a3bcae
suraj patel <sp9589890205@gmail.com>
2025-02-12 11:53:15 +0530

    [Suraj] added Convert a Set to a Sorted List

---
+++ Day_02/src/main/java/com/collections/set/settosortedlist/Main.java
@@ -0,0 +1,23 @@
+package com.collections.set.settosortedlist;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import static com.collections.set.settosortedlist.SetToSortedList.convertToSortedList;
+
+
+public class Main {
+    public static void main(String[] args) {
+        //Create and populate HashSet
+        Set<Integer> set = new HashSet<>(Arrays.asList(5, 3, 9, 1));
+        System.out.println(set);
+        //Convert to sorted list
+        List<Integer> sortedList = convertToSortedList(set);
+
+        //Print result
+
+        System.out.println("Output: " + sortedList);
+    }
+}

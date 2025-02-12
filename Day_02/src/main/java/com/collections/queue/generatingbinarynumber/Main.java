5ad32a50118233ac2aaa7cfef72f5befd58eabb5
suraj patel <sp9589890205@gmail.com>
2025-02-12 11:42:15 +0530

    [Suraj] added Generate Binary Numbers Using a Queue

---
+++ Day_02/src/main/java/com/collections/queue/generatingbinarynumber/Main.java
@@ -0,0 +1,15 @@
+package com.collections.queue.generatingbinarynumber;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        int number =5;
+        //object of class
+        GeneratingBinaryNumber generatingBinaryNumber = new GeneratingBinaryNumber();
+        //method call and store result
+        List<String> answer = generatingBinaryNumber.generateBinaryNumbers(number);
+        //displaying the result
+        System.out.println(answer);
+    }
+}

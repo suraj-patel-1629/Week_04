8616416734e03ff615a0f6f242783dbcf79b7661
suraj patel <sp9589890205@gmail.com>
2025-02-13 16:30:08 +0530

    [Suraj] Added . Store and Retrieve Primitive Data

---
+++ Day_03/src/main/java/com/streams/datastream/Main.java
@@ -0,0 +1,9 @@
+package com.streams.datastream;
+
+public class Main {
+    public static void main(String[] args) {
+        DataStreams.storeStudentDetails();
+
+        DataStreams.retrieveStudentDetails();
+    }
+}

e82328d6ad1d6648d41047302c8df0aab578bee0
suraj patel <sp9589890205@gmail.com>
2025-02-14 22:53:47 +0530

    [Suraj] added   Exception Propagation in Methods

---
+++ Day_04/src/main/java/com/exceptionhandling/exceptionpropagationinmethods/ExceptionPropagationExample.java
@@ -0,0 +1,30 @@
+//8.Exception Propagation in Methods
+//Problem Statement:
+//Create a Java program with three methods:
+//method1(): Throws an ArithmeticException (10 / 0).
+//method2(): Calls method1().
+//main(): Calls method2() and handles the exception.
+//Expected Behavior:
+//The exception propagates from method1() → method2() → main().
+//Catch and handle it in main(), printing "Handled exception in main".
+
+
+package com.exceptionhandling.exceptionpropagationinmethods;
+
+public class ExceptionPropagationExample {
+    static void method1() {
+        throw new ArithmeticException("Division by zero");
+    }
+
+    static void method2() {
+        method1();
+    }
+
+    public static void main(String[] args) {
+        try {
+            method2();
+        } catch (ArithmeticException e) {
+            System.out.println("Handled exception in main");
+        }
+    }
+}
